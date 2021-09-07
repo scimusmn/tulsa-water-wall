47a45b37998c13cfbdc7c3fd4674f7939d49c08c
bkennedy <bkennedy@smm.org>
2021-09-07 13:24:45 -0500

    feat: Remove purgecss
    This library isn't needed any longer. I threw it at another bug and it
    wasn't the right solution. Removing for simplicity.

--- packages/client/gatsby-config.js
+++ packages/client/gatsby-config.js
@@ -22,20 +22,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-purgecss',
-      options: {
-        printRejected: false, // Print removed selectors and processed file names
-        develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-        purgeCSSOptions: {
-          // https://purgecss.com/configuration.html#options
-          // safelist: ['safelist'], // Don't remove this selector
-        },
-      },
-    },
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Tulsa Water Wall - Drawing',
