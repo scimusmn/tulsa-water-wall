c2a084369522fb782920cb6360b295bc102ca41e
bkennedy <bkennedy@smm.org>
2021-11-01 15:19:42 -0500

    feat: Define the drawing area and set bg color

--- packages/client/src/components/DrawingArea/index.js
+++ packages/client/src/components/DrawingArea/index.js
@@ -1,8 +1,13 @@
+//
+// Drawing area
+//
+// Set up the dimensions of the canvas
+//
 import React from 'react';
 import DrawingCanvas from '@components/DrawingArea/DrawingCanvas';
 
 const Drawing = () => (
-  <div className="bg-yellow mt-5 h-5/6">
+  <div className="bg-white mt-5 h-5/6">
     <DrawingCanvas pixelsWide={120} pixelsTall={80} pixelMargin={2} />
   </div>
 );
