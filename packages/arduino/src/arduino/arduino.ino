8312807378d47a1b895420ea26a4d1b3d84116d3
sanine-a <sanine.not@pm.me>
2021-12-01 14:55:49 -0600

    add update.bat and adjust arduino vertical resolution

--- packages/arduino/src/arduino/arduino.ino
+++ packages/arduino/src/arduino/arduino.ino
@@ -11,7 +11,10 @@ ShiftRegisterManager registers;
 #define CLOCK_PIN 10
 #define DATA_PIN 12
 #define LINE_SIZE 15
-#define N_LINES 40
+#define N_LINES 80
+
+#define SKIPPING true
+#define SKIP_EVERY 4
 
 #define LINE_ON 10
 #define LINE_OFF 1
@@ -21,41 +24,43 @@ unsigned int current_line = 0;
 
 
 void beginDrawing() {
-   current_line = 0;
+  current_line = 0;
 }
 
 void decodeLine(const char* line) {
-   if (current_line >= N_LINES ||
-       current_line % 2) // halve the vertical resolution
-      return;
-   unsigned int size = decode_base64(line, drawing[current_line]);
-   serial.send("decoded-bytes", (int) size);
-   current_line += 1;
-   serial.send("acknowledge-line");
+  if (current_line >= N_LINES)
+    return; //overflow protection
+  unsigned int size = decode_base64(line, drawing[current_line]);
+  serial.send("decoded-bytes", (int) size);
+  current_line += 1;
+  serial.send("acknowledge-line");
 }
 
 void publishDrawing() {
-   serial.send("rx-ready", "0");
-   for (int i=0; i<N_LINES; i++) {
-      registers.write(drawing[i], LINE_SIZE);
-      delay(LINE_ON);
-      registers.clear(LINE_SIZE);
-      delay(LINE_OFF);
-   }
-   serial.send("rx-ready", "1");
+  serial.send("rx-ready", "0");
+  for (int i=0; i<N_LINES; i++) {
+    if (SKIPPING && !(i % SKIP_EVERY))
+      continue;
+    
+    registers.write(drawing[i], LINE_SIZE);
+    delay(LINE_ON);
+    registers.clear(LINE_SIZE);
+    delay(LINE_OFF);
+  }
+  serial.send("rx-ready", "1");
 }
 
 
 
 void setup() {
-   registers.setup(LATCH_PIN, CLOCK_PIN, DATA_PIN, LINE_SIZE);
+  registers.setup(LATCH_PIN, CLOCK_PIN, DATA_PIN, LINE_SIZE);
 
-   serial.setup();
-   serial.addCallback("begin-drawing", beginDrawing);
-   serial.addCallback("line", decodeLine);
-   serial.addCallback("publish-drawing", publishDrawing);
+  serial.setup();
+  serial.addCallback("begin-drawing", beginDrawing);
+  serial.addCallback("line", decodeLine);
+  serial.addCallback("publish-drawing", publishDrawing);
 
-   serial.send("rx-ready", "1");
+  serial.send("rx-ready", "1");
 }
 
 
@@ -64,12 +69,12 @@ const int OFF_TIME = 250;
 const byte line[LINE_SIZE];
 
 void loop() {
-   /*   memset(line, 0xff, LINE_SIZE);
-   registers.write(line, LINE_SIZE);
-   delay(ON_TIME);
-   memset(line, 0x00, LINE_SIZE);
-   registers.write(line, LINE_SIZE);
-   delay(OFF_TIME);*/
-   serial.update();
+  /*   memset(line, 0xff, LINE_SIZE);
+       registers.write(line, LINE_SIZE);
+       delay(ON_TIME);
+       memset(line, 0x00, LINE_SIZE);
+       registers.write(line, LINE_SIZE);
+       delay(OFF_TIME);*/
+  serial.update();
 
 }
